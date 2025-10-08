4e4c4122aba3b4621001d33b6ec0b14e05c37274
BrunaLuisaCRS <bruna2212br1@gmail.com>
2025-10-08 20:35:36 -0300

    parcial2

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
+    console.log(request.url);
+    console.log(request,method);
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text/plain');
     response.end("Olá pikomon!");
