bf8749153dfb097149ea27c08a9ce104b54ba204
BrunaLuisaCRS <bruna2212br1@gmail.com>
2025-10-12 18:42:57 -0300

    js, .env, jsnon, gitignore e concertos html

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const http = require('http');
+const env = require('dotenv/config');
 
-const server = http.createServer((request, response) => {
-    console.log(request.url);
-    console.log(request,method);
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
+
+const server = http.createServer((request, response) =>{
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text/plain');
-    response.end("Olá pikomon!");
+    response.end('Olá mundo!');
 })
 
-server.listen(300, '0.0.0.0', () => {console.log("Servidor iniciado");})
+server.listen(PORT, HOST, ()=> {
 
-
+    console.log('Servidor iniciado');
+})
