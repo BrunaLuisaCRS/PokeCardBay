235e150a1d7278177f870e51c256283108c6688c
BrunaLuisaCRS <bruna2212br1@gmail.com>
2025-10-21 18:46:26 -0300

    Home, Login e Cadastro com html e css prontos (home sóa base e o carrossel) além de mais organização nas pastas.

--- index.js
+++ index.js
@@ -6,11 +6,18 @@ const HOST = process.env.HOST;
 
 const server = http.createServer((request, response) =>{
     response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/plain');
-    response.end('Olá mundo!');
+    response.setHeader('Content-Type', 'text/html');
+    response.end(`<html>
+        <body>
+        <h1> HTML!! </h1>
+        </body>
+        </html>
+        
+        `);
 })
 
 server.listen(PORT, HOST, ()=> {
 
     console.log('Servidor iniciado');
 })
+
