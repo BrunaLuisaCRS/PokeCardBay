f81d0a92a30a53bef203e81bde1dac69488dffe5
AliciaNascimento05 <alicianascimento.sn@gmail.com>
2025-11-27 00:16:41 -0300

    Login resolvido

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -3,7 +3,7 @@ import User from '#models/user'
 // 1. Importe seu validador
 import { createUserValidator } from '#validators/user_validator' 
 // 2. Importe o serviço de hash
-import hash from '@adonisjs/core/services/hash' 
+//import hash from '@adonisjs/core/services/hash' 
 
 export default class UsersController {
   
@@ -18,13 +18,14 @@ export default class UsersController {
       const data = await request.validateUsing(createUserValidator)
 
       // 2. Criar o hash USANDO SCRYPT
-      const hashedPassword = await hash.use('scrypt').make(data.password)
+      //const hashedPassword = await hash.use('scrypt').make(data.password)
 
       // 3. Criar o usuário USANDO A SENHA HASHEADA
       await User.create({
         full_name: data.full_name,
         email: data.email,
-        password: hashedPassword  // <-- PONTO CRÍTICO!
+        password: data.password, // <-- senha pura, sem hash manual
+        //password: hashedPassword  // <-- PONTO CRÍTICO!
       })
 
       // 6. Deu tudo certo, redirecione para o login
